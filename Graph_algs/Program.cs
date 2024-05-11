78741e4aa3c24470212d585022293160ada37893
Nikita Goltsev <nik.goltsev.97@bk.ru>
2024-05-11 17:23:57 +0300

    Trial_of_graphs

---
+++ Graph_algs/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+class Program
+{
+    public  static  void Main(string[] args)
+    {
+        Console.WriteLine();
+
+
+    }
+
+
+}
---
+++ Graph_algs/graph_write.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Graph_list_sm
+{
+    private int _vertices;
+    private List<int>[] _adjacencyList;
+
+    public Graph(int vertices) // Создание графа с v вершинами
+    {
+        _vertices = vertices;
+        _adjacencyList = new List<int>[vertices];
+        for (int i = 0; i < vertices; i++)
+            _adjacencyList[i] = new List<int>();
+    }
+}
+
